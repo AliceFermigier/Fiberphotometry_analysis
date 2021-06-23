89661f75dc902051ae6e3860c39bb8c95fd98907
AliceFermigier <76163431+AliceFermigier@users.noreply.github.com>
2021-06-23 17:26:31 +0200

    Create Plot_tracking.py

---
+++ Plot_tracking.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 18 11:41:03 2021
+
+Plot tracking
+
+@author: Alice Fermigier
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df_tracking = pd.read_csv('E:\\Alice\\Fiber\\202103_CDHFDdvHPC\\20210302_ORM\\Test1h\\CD1_tracking.csv')
+
+fig3 = plt.figure(figsize=(20,20))
+ax2 = fig3.add_subplot(111)
+
+p1, = ax2.plot('Item2.X', 'Item2.Y', linewidth=.5, color='black', data=df_tracking)
