55b081e9d0a5c5d47e159e362f5909d1a2799fb5
AliceFermigier <76163431+AliceFermigier@users.noreply.github.com>
2026-01-12 15:42:29 +0100

    Minor changes

--- scripts/script_preprocess.py
+++ scripts/script_preprocess.py
@@ -43,8 +43,8 @@ from scripts.loader import experiment_path, analysis_path, data_path, proto_df, 
 # 1 - PREPROCESSING
 #####################
 
-exp = 'EPM'
-dual_color = False
+exp = 'Screening_1'
+dual_color = True
 # Step 1: Create main experiment folder and session subfolders
 exp_path = nom.setup_experiment_directory(analysis_path, exp)
 print(f"Experiment directory created at: {exp_path}")
@@ -120,8 +120,8 @@ for mouse, batch in zip(subjects_df['Subject'], subjects_df['Batch']):
 # 1.3 - Open artifacted data and score artifacts (when big artifacts due to patch cord disconnection)
 
 #------------------#
-mouse = '829'
-batch = 2
+mouse = '944'
+batch = 1
 filecode = f'{exp}_{mouse}'
 #------------------#
 
