341030c0ebb771dc830f607327bd62d6ebf875da
AliceFermigier <76163431+AliceFermigier@users.noreply.github.com>
2026-03-21 08:46:17 +0100

    Update video_alignment.py

--- modules/behaviour/video_alignment.py
+++ modules/behaviour/video_alignment.py
@@ -62,7 +62,7 @@ def create_overlay_frame(index, fiberbehav_df, behavior_cols, window):
     # Setup figure
     n_behavior = len(behavior_cols)
     has_speed = 'Speed' in fiberbehav_df.columns
-    has_560 = 'Denoised 560 dFF' in fiberbehav_df.columns
+    has_560 = '560 dFF' in fiberbehav_df.columns
     height_ratios = [8]
     if has_560:
         height_ratios += [8]
@@ -92,17 +92,17 @@ def create_overlay_frame(index, fiberbehav_df, behavior_cols, window):
     t = window_df['Time(s)']
 
     # Plot fiber signal
-    axs[0].plot(t, window_df['Denoised dFF'], color='black')
+    axs[0].plot(t, window_df['dFF'], color='darkgreen')
     axs[0].set_ylabel('465 dFF')
-    axs[0].set_ylim(max(-0.3, fiberbehav_df['Denoised dFF'].min()), min(1.5, fiberbehav_df['Denoised dFF'].max()))
-    axs[0].axvspan(center_time, end_time, color='white', alpha=0.95, zorder=10)
+    axs[0].set_ylim(max(-0.3, fiberbehav_df['dFF'].min()), min(1.5, fiberbehav_df['dFF'].max()))
+    axs[0].axvspan(center_time, end_time, color='white', zorder=10)
 
     # Optional: plot 560nm dFF
     if has_560:
-        axs[1].plot(t, window_df['Denoised 560 dFF'], color='darkorange')
+        axs[1].plot(t, window_df['560 dFF'], color='darkorange')
         axs[1].set_ylabel('560 dFF')
-        axs[1].set_ylim(max(-0.5, fiberbehav_df['Denoised dFF'].min()), min(1.5, fiberbehav_df['Denoised dFF'].max()))
-        axs[1].axvspan(center_time, end_time, color='white', alpha=0.95, zorder=10)
+        axs[1].set_ylim(max(-0.5, fiberbehav_df['560 dFF'].min()), min(1.5, fiberbehav_df['560 dFF'].max()))
+        axs[1].axvspan(center_time, end_time, color='white', zorder=10)
     
     # Plot behaviors
     behavior_colors_path = Path(project_root) / "modules/behaviour/behaviour_colors.json"
@@ -129,7 +129,7 @@ def create_overlay_frame(index, fiberbehav_df, behavior_cols, window):
         axs[i + pad].set_ylabel(behavior, rotation=0, labelpad=10, va='center', ha='right')
         axs[i + pad].spines['left'].set_visible(False)
         axs[i + pad].set_ylim(0.1, 1.1)
-        axs[i + pad].axvspan(center_time, end_time, color='white', alpha=0.95, linewidth=3, zorder=10)
+        axs[i + pad].axvspan(center_time, end_time, color='white', linewidth=3, zorder=10)
 
     # Optional: plot speed
     if has_speed:
@@ -138,7 +138,7 @@ def create_overlay_frame(index, fiberbehav_df, behavior_cols, window):
         axs[-1].set_ylim(global_min, global_max)
         axs[-1].plot(t, window_df['Speed'], color='black')
         axs[-1].set_ylabel('Speed')
-        axs[-1].axvspan(center_time, end_time, color='white', alpha=0.95, zorder=10)
+        axs[-1].axvspan(center_time, end_time, color='white', zorder=10)
         
     # Hide x-axis labels and bottom spines for all but the last axis
     for ax in axs:
